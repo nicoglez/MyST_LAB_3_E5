a57dbc295143d7018566dc5723c4f9a9173bd7cb
Nicolas Gonzalez <nicolasvazquez1@outlook.es>
2022-10-07 08:37:14 -0500

    Add files and fix bugs

--- functions.py
+++ functions.py
@@ -17,9 +17,9 @@ def f_leer_archivo(param_archivo: str) -> pd.DataFrame:
     # Renombrar columnas si no estan en         
     data.rename(columns=dict(zip(data.columns.values, col_names)), inplace=True) 
     # Quitar rows que no necesitamos, quitando aquellas que no afectan la posicion, de ordenes para adelante (español o ingles)
-    data = data.iloc[:np.argmax(data.iloc[:, 0].values == "Orders"), 0:-2] \
+    data = data.iloc[:np.argmax(data.iloc[:, 0].values == "Orders"), 0:-3] \
                     if sum(data.iloc[:, 0].values == "Orders")==1 \
-                    else data.iloc[:np.argmax(data.iloc[:, 0].values == "Órdenes"), 0:-2] 
+                    else data.iloc[:np.argmax(data.iloc[:, 0].values == "Órdenes"), 0:-3]
     # Quitar blanks: comisiones y swaps
     data = data[data.columns.drop(["Commission", "Swap"])]
         
