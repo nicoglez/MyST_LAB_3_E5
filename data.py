9e2d81a6c77e67e54746891fa1eeb07092c6627e
Nicolas Gonzalez <nicolasvazquez1@outlook.es>
2022-10-17 22:33:36 -0500

    add Data

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from functions import f_leer_archivo
+from functions import f_leer_archivo, f_columnas_tiempos, f_columnas_pips
 
 # Paths de cada integrante
 path_Santiago = "files/ReportHistory-5503127_SRR.xlsx"
@@ -11,3 +11,17 @@ data_Santiago = f_leer_archivo(path_Santiago)
 data_Nicolas = f_leer_archivo(path_Nicolas)
 data_Ivan = f_leer_archivo(path_Ivan)
 data_Carolina = f_leer_archivo(path_Carolina)
+
+# Agregar columnas con funciones creadas
+
+# Agregar columnas de tiempos
+data_Santiago = f_columnas_tiempos(data_Santiago)
+data_Nicolas = f_columnas_tiempos(data_Nicolas)
+data_Ivan = f_columnas_tiempos(data_Ivan)
+data_Carolina = f_columnas_tiempos(data_Carolina)
+
+# Agregar columnas de pips
+data_Santiago = f_columnas_pips(data_Santiago)
+data_Nicolas = f_columnas_pips(data_Nicolas)
+data_Ivan = f_columnas_pips(data_Ivan)
+data_Carolina = f_columnas_pips(data_Carolina)
